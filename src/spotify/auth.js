e45b85628837d73f6d75f57b3160bdf66ca1e85b
Aditya Chauhan <chauhan.aditya7051@gmail.com>
2026-08-19 00:58:05 +0530

    updated for vercel

--- src/spotify/auth.js
+++ src/spotify/auth.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 const REDIRECT_URI =
-  window.location.origin + import.meta.env.BASE_URL;
+  window.location.origin + '/callback';
 
 export function getAccessToken() {
   return localStorage.getItem('spotify_access_token');
